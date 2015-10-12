4fcddcd1e40b4ead06199b1c175b77b5429ad46d
Terry You <microvoyager@hotmail.com>
2015-10-12 16:59:00 +0800

    can separate to 2 dataset 1.train 2.test , add correct ration
    因為一開始的方法是先切2/3, 1/3在去做random亂數加入train set and test set
    
    這樣會造成後面類別的資料數較少，前面類別的資料數較多，這樣在做運算時有時候兩圓相交的圖案下，會因為第一類是完整的圓，第二類可能只有半圓，造成可以找到solution,but
    in fact 應該要無法找到solution才對，因此改成一開始就先random
    copy到另外一個array_temp，再用array_temp去分成兩個set，等到訓練完後用test set 拿train完的
    最終值再去跑一次判斷，但這次不做調整，只記錄correct次數，之後再return回次數作correct ration

--- src/cnnhw1/cnnhw1.java
+++ src/cnnhw1/cnnhw1.java
@@ -9,8 +9,10 @@ import javax.swing.*;
 public class cnnhw1 extends JFrame{ //目前這個程式碼主要先測試感知機1
 
 	static ArrayList<float[]> array_input = new ArrayList<float[]>();
-//	static ArrayList<float[]> array_calculate = new ArrayList<float[]>();
-	
+	static ArrayList<float[]> array_temp = new ArrayList<float[]>();	
+	static ArrayList<float[]> array_train = new ArrayList<float[]>();
+	static ArrayList<float[]> array_test = new ArrayList<float[]>();
+		
 //	static Scanner input = new Scanner(System.in);
 	
 	private static float eastxbound=400.0f;
@@ -56,7 +58,7 @@ public class cnnhw1 extends JFrame{ //目前這個程式碼主要先測試感知
 		frame.add(trypaint);//add paint(class) things in to the frame		
 	}
 
-	private static void printarraylist(ArrayList<float[]> array_input){
+	private static void printarraylist(ArrayList<float[]> array_input,ArrayList<float[]> array_temp,ArrayList<float[]> array_train,ArrayList<float[]> array_test){
 		for(int i=0;i<array_input.size();i++){
 			for(int j=0;j<array_input.get(i).length;j++){
 			System.out.print(array_input.get(i)[j] + "\t");
@@ -69,39 +71,108 @@ public class cnnhw1 extends JFrame{ //目前這個程式碼主要先測試感知
 		System.out.println("output threshold & weight");
 		System.out.printf("%f\n",threshold);//use c language to print prevant loss ....??
 		for(int w=0;w<initial.length;w++){
-		System.out.printf("%f\n",initial[w]);//use c language to print prevant loss ....??
+			System.out.printf("%f\n",initial[w]);//use c language to print prevant loss ....??
 		}		
 	}
 
-	private static float sumall(ArrayList<float[]> array_input,float[] initial,float caltemp,float threshold,float sum,int x0,int xn){
-		for(int w=0;w<(array_input.get(xn).length-1);w++){//要用的只有前兩個 最後一個是desire
-			sum += (initial[w]) * (array_input.get(xn)[w]);//做運算時把array內的職先轉乘float
+	private static void putInputToTemp(ArrayList<float[]> array_input){
+		int arrayinputamount=array_input.size();
+		Random rand=new Random();
+		while(arrayinputamount!=0){
+			int n=rand.nextInt(arrayinputamount)+0;
+			array_temp.add(array_input.get(n));
+			arrayinputamount--;
+		}
+	}
+	
+	private static void separateTemp(ArrayList<float[]> array_temp){
+		int totalamount=array_temp.size();
+		int tocalamount=(totalamount*2)/3;
+		int totestamount=totalamount-tocalamount;
+
+		while(tocalamount!=0){
+			array_train.add(array_temp.get(0));
+			array_temp.remove(0);
+			tocalamount--;
+		}
+		while(totestamount!=0){
+			array_test.add(array_temp.get(0));
+			array_temp.remove(0);
+			totestamount--;
+		}	
+	}
+	
+	private static void printCorrectRatio(float[] initial,float threshold,ArrayList<float[]> array_test){
+		int testcorrect=testCheck(initial,threshold,array_test);//call function(testCheck) to get correct
+		int testamount=array_test.size();
+		System.out.println("test amount is :"+testamount);
+		System.out.println("right palce amount is :"+testcorrect);
+		System.out.println("Test correct ratio is : "+((float)testcorrect/(float)testamount)*100+"%");	
+	}
+	
+	private static float sumall(ArrayList<float[]> array,float[] initial,float caltemp,float threshold,float sum,int x0,int xn){
+		for(int w=0;w<(array.get(xn).length-1);w++){//要用的只有前兩個 最後一個是desire
+			sum += (initial[w]) * (array.get(xn)[w]);//做運算時把array內的職先轉乘float
 		}
 		caltemp=x0*threshold;
 		sum += caltemp;	//閥值跟x0相乘最後再做		
 		return sum;
 	}	
 	
-	private static	float equation(float w0,float w1,float threshold,float x){
+	private static	float getY(float w0,float w1,float threshold,float x){
 		float y;
 		y=(threshold/w1)-(w0/w1)*(x/16);
 		return y;
+	}
+	
+	private static int testCheck(float[] initial,float threshold,ArrayList<float[]> array_test){
+		//run calculate again but this time just count correcttimes
+		float caltemp=0f;
+		float judge=0f;
+		int x0=-1;
+		int testcorrect=0;
+		int testamount=array_test.size();
+		int wrongflag=0;
+		for(int i=0;i<testamount;i++){
+			float sum=0f;
+				for(int w=0;w<(array_test.get(i).length-1);w++){//要用的只有前兩個 最後一個是desire
+					sum += (initial[w]) * (array_test.get(i)[w]);//做運算時把array內的職先轉乘float
+				}
+				caltemp=x0*threshold;
+				sum += caltemp;	//閥值跟x0相乘最後再做		
+
+				judge = Math.signum(sum);//use math's sign function return value
+				
+				if (judge!=array_test.get(i)[array_test.get(i).length-1]&&judge>=0){
+					wrongflag++;//just count,do nothing
+				}
+				else if(judge!=array_test.get(i)[array_test.get(i).length-1]&&judge<0){
+					wrongflag++;//just count,do nothing
+				}
+				else{
+					testcorrect++;
+				}				
+		}
+		return testcorrect;
 	}
 
 
 	/*	 
 	 *  1.load file & use split then trans to float wiz token2,then store into array
 	 *  2.class defination
-	 *  3.get initial information
-	 * 	4.do calculate & set some flag 
-	 *  5.genarate line equation
-	 *  6.generate gui 
+	 *  3.copy array_input to array_temp with random, then separate 2/3 as train dataset 1/3 as test set
+	 *  4.get initial information
+	 * 	5.do calculate & set some flag 
+	 *  6.genarate line equation
+	 *  7.check correct ratio(call function in printCorrctRatio) then print correctratio
+	 *  8.generate gui 
 	 */
 	public static void main(String[] args) throws IOException{
-		/*
+	
+	/*
 	 * 1.load file & use split then trans to float wiz token2,then store into array
 	 */
-		String Filename = "E:\\1041\\cnn\\HW1\\dataset\\2Hcircle1.txt";
+		String Filename = "E:\\1041\\cnn\\HW1\\dataset\\2Circle2.txt";
 		FileReader fr = new FileReader(Filename); 
 		BufferedReader br = new BufferedReader(fr);//在br.ready反查輸入串流的狀況是否有資料
 
@@ -138,8 +209,16 @@ public class cnnhw1 extends JFrame{ //目前這個程式碼主要先測試感知
 		classify(array_input);//call function to classify
 		
 	/*
-	 * 3.get initial information. maybe will change to top in the future. 
-	 * 先放在這裡
+	 * 3.copy array_input to array_temp with random, then separate 2/3 as train dataset 1/3 as test set
+	 */
+		putInputToTemp(array_input);// copy to temp with random
+		
+		separateTemp(array_temp);//separate to train and test set,set 2/3 as train set 1/3 as test set
+
+
+	/*
+	 * 4.get initial information. maybe will change to top in the future. 
+	 * 
 	 */		
 		
 		// Erstellung Array vom Datentyp Object, Hinzufügen der Komponenten		
@@ -162,43 +241,37 @@ public class cnnhw1 extends JFrame{ //目前這個程式碼主要先測試感知
 		float threshold= Float.parseFloat(usrthreshold.getText());
 		System.out.println("Your threshold is "+ threshold);
 		System.out.println("-----start to do calculate-----");
-	
+
 	/*
-	 * 	4.do calculate & set some flag
+	 * 	5.do calculate & set some flag
 	 */
 		float[] initial={1.0f,-1.0f};
 		int x0=-1;
-
 		float caltemp=0f;
 		float judge=0f;
 		int xn=0;
-		int dataamount = array_input.size();
+		int dataamount = array_train.size();
 		int correctcount=0;
 		int correctflag = 0;
 		
 		whileloop:
 			while(looptimes!=0){
 				float sum=0f;//db2 sum is a register and you have to reset to zero.
-				sum  =sumall(array_input,initial,caltemp,threshold,sum,x0,xn);//call function to sum all things
+				sum  =sumall(array_train,initial,caltemp,threshold,sum,x0,xn);//call function to sum all things
 				
 				judge = Math.signum(sum);//use math's sign function return value
 		
-				if (judge!=array_input.get(xn)[array_input.get(xn).length-1]&&judge>=0){
-					for(int w=0;w<(array_input.get(xn).length-1);w++){//要用的只有前兩個 最後一個是desire
-						initial[w] -= studyrate*array_input.get(xn)[w];
-						
-//						array_input.get(xn)[w]=initial[w];
+				if (judge!=array_train.get(xn)[array_train.get(xn).length-1]&&judge>=0){
+					for(int w=0;w<(array_train.get(xn).length-1);w++){//要用的只有前兩個 最後一個是desire
+						initial[w] -= studyrate*array_train.get(xn)[w];
 					}
 					threshold -= studyrate*x0;			
 					correctcount=0;
-					System.out.printf("---more 0 : (%f,%f)---\n", initial[0],initial[1]);
 				}
-				else if(judge!=array_input.get(xn)[array_input.get(xn).length-1]&&judge<0){
-					for(int w=0;w<(array_input.get(0).length-1);w++){//要用的只有前兩個 最後一個是desire
-						initial[w] += studyrate*array_input.get(xn)[w];
-//						array_input.get(xn)[w]=initial[w];
+				else if(judge!=array_train.get(xn)[array_train.get(xn).length-1]&&judge<0){
+					for(int w=0;w<(array_train.get(0).length-1);w++){//要用的只有前兩個 最後一個是desire
+						initial[w] += studyrate*array_train.get(xn)[w];
 					}
-					System.out.printf("---small 0 : (%f,%f)---\n", initial[0],initial[1]);
 					threshold += studyrate*x0;
 					correctcount=0;
 				}
@@ -206,7 +279,7 @@ public class cnnhw1 extends JFrame{ //目前這個程式碼主要先測試感知
 					correctcount++;
 				}
 				
-				printthresholdandinitial(threshold,initial);
+//				printthresholdandinitial(threshold,initial);
 				
 				if(correctcount==dataamount-1){
 					correctflag=1;
@@ -221,20 +294,28 @@ public class cnnhw1 extends JFrame{ //目前這個程式碼主要先測試感知
 				}//xn 歸零重頭開始算
 				looptimes--;//looptimes countdown
 			}
-		System.out.println("************"+dataamount);
-		System.out.println("++++++++++++"+correctcount);
+
 		
+		flagdecide(correctflag);//call function to decide
 		
-		flagdecide(correctflag);//call function to decide 
-		
-		float liney1=equation(initial[0],initial[1],threshold,eastxbound);
-		float liney2=equation(initial[0],initial[1],threshold,westxbound);
+
+	/*
+	 * 6.genarate line equation
+	 */
+		float liney1=getY(initial[0],initial[1],threshold,eastxbound);
+		float liney2=getY(initial[0],initial[1],threshold,westxbound);
 		
 	/*
-	 * 5.generate gui
+	 * 7.check correct ratio(call function in printCorrctRatio) then print correctratio
+	 */
+		printCorrectRatio(initial,threshold,array_test);
+
+
+	/*
+	 * 8.generate gui
 	 */		
 		
-		genarateframe(array_input,initial,threshold,liney1,liney2,framesizex,framesizey);//call function to genarate frame
+		genarateframe(array_train,initial,threshold,liney1,liney2,framesizex,framesizey);//call function to genarate frame
 
 	}	
 
